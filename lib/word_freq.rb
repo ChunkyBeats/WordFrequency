b7f126523d4ff7fec018613d2a8bfbbcd580c6e4
Cory Ribson <student@epicodus.com>
2015-04-24 09:48:10 -0700

    Finalized specs for .rb files

--- lib/word_freq.rb
+++ lib/word_freq.rb
@@ -1,33 +1,23 @@
-# class String
-#   define_method(:word_freq) do |inString|
-#     key_word = self
-#     input_string = inString
-#
-#     key_array = self.split("")
-#     string_array = input_string.split()
-#     word_count = 0
-#     string_array.each() do |index|
-#       test string_array.split("")
-#       if key_array == string_
-#   end
-# end
-#
-
 
 class String
   define_method(:word_freq) do |inString|
     key_word = self
-    input_string = inString
-
-    string_array = input_string.split()
     word_count = 0
-    string_array.each() do |index|
-      if key_word == index
-        word_count = word_count.+(1)
+    if key_word == "" || inString == ""
+      word_count = "Either no key, or no string was found"
+    else
+      input_string = inString
+
+      string_array = input_string.split()
+      word_count = 0
+      string_array.each() do |index|
+        if key_word == index
+          word_count = word_count.+(1)
+        end
       end
-    end
-    if word_count == 0
-      word_count = "No instances of your key word were found"
+      if word_count == 0
+        word_count = "No instances of your key word were found"
+      end
     end
     word_count
   end
--- spec/word_freq_spec.rb
+++ spec/word_freq_spec.rb
@@ -5,4 +5,16 @@ describe('String#word_freq') do
   it('will take user input for a word and check how often it appears in a user defined string') do
     expect(("balloon").word_freq("I took a balloon into a hot air balloon")).to(eq(2))
   end
+
+  it('will check if no instances of the key were found') do
+    expect(("tigers").word_freq("I saw penguins at the zoo")).to(eq("No instances of your key word were found"))
+  end
+
+  it('will check if any input was found for the key word') do
+    expect(("").word_freq("Try again")).to(eq("Either no key, or no string was found"))
+  end
+
+  it('will check if any input was for the the string') do
+    expect(("bears").word_freq("")).to(eq("Either no key, or no string was found"))
+  end
 end
